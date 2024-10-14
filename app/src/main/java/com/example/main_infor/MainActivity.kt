1178c123d3434272d54e005d9dd3d6b5d16960dc
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-10-14 15:47:57 +0700

    3:47

--- app/src/main/java/com/example/main_infor/MainActivity.kt
+++ app/src/main/java/com/example/main_infor/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 //        Nếu muốn chạy activity không thì comment lại
-//        val intent = Intent(this, InforChat::class.java)
-//        startActivity(intent)
+        val intent = Intent(this, InforChat::class.java)
+        startActivity(intent)
     }
 }
